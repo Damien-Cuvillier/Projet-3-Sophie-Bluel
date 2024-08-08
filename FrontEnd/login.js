b81de50ece9cb81e82b8aae40ff0d97bc84ee753
Damien Cuvillier <damien.cuvillier.37@gmail.com>
2024-08-08 16:58:24 +0200

    Modification des fichiers pour le bon fonctionnement de la connexion et redirection sur la page index.html

--- FrontEnd/login.js
+++ FrontEnd/login.js
@@ -5,9 +5,9 @@ document.addEventListener('DOMContentLoaded', () => {
     form.addEventListener('submit', async (event) => {
         event.preventDefault();
 
-        const email = document.getElementById('email');
-        const password = document.getElementById('password');
-
+        const email = document.getElementById('email').value;
+        const password = document.getElementById('password').value;
+        
         try {
             const response = await fetch('http://localhost:5678/api/users/login', {
                 method: 'POST',
